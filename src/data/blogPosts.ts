fd0b9fe407e022f217739f9399826cf7a4e035b1
finnsk-engineering <finn@finnsk.com>
2025-12-30 22:19:18 +1100

    chore: automated site generation from architect prompt

---
+++ src/data/blogPosts.ts
@@ -0,0 +1,26 @@
+export const blogPosts = [
+  {
+    slug: '/go-hard-and-go-big-how-australia-got-solar-panels-onto-one-in-every-three-houses',
+    title: '‘Go hard and go big’: How Australia got solar panels onto one in every three houses',
+    image: 'https://renewableindustries.co/wp-content/uploads/2024/06/2500-768x461.avif',
+    excerpt: 'Households have continued to use state help that was first created more than a decade ago...',
+    date: 'June 24, 2024',
+    category: 'Blog',
+  },
+  {
+    slug: '/australia-has-the-best-solar-resources-in-the-world-we-should-learn-how-to-make-our-own-panels',
+    title: 'Australia has the best solar resources in the world: We should learn how to make our own panels',
+    image: 'https://renewableindustries.co/wp-content/uploads/2024/06/person-near-alternative-energy-plant-1-scaled-1-768x512.webp',
+    excerpt: 'Australia has the best solar resources in the world, the best innovators in the world, and leads the world in solar rooftop installations...',
+    date: 'June 24, 2024',
+    category: 'Blog',
+  },
+  {
+    slug: '/battery-storage-comes-to-the-fore-as-solar-power-generation-reaches-new-highs-across-australia',
+    title: 'Battery storage comes to the fore as solar power generation reaches new highs across Australia',
+    image: 'https://renewableindustries.co/wp-content/uploads/2024/06/34e8c2d2c9ee46e64cc441ddd600edbf-768x432.avif',
+    excerpt: 'It is, as Jason Froud knows all too well, a little-known but fundamentally important aspect of the power system...',
+    date: 'June 24, 2024',
+    category: 'Blog',
+  },
+];
